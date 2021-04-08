d55276ae7f87b2d4d0794f98376c51fbe7170eba
David Kipnis <pankimatick@gmail.com>
2021-04-07 22:44:20 -0700

    Initial Commit

---
+++ FitCheck V.0.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitCheck V.0.1
+//
+//  Created by David Kipnis on 4/7/21.
+//  Copyright © 2021 David Kipnis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
