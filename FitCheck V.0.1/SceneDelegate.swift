23233eaf6376065b1ccfda13f09a13ad1ea05fc6
David Kipnis <pankimatick@gmail.com>
2021-04-22 10:48:47 -0700

    Nav update

--- FitCheck V.0.1/SceneDelegate.swift
+++ FitCheck V.0.1/SceneDelegate.swift
@@ -2,8 +2,8 @@
 //  SceneDelegate.swift
 //  FitCheck V.0.1
 //
-//  Created by David Kipnis on 4/7/21.
-//  Copyright © 2021 David Kipnis. All rights reserved.
+//  Created by David Kipnis and Hannah Buzard on 4/7/21.
+//  Copyright © 2021 David Kipnis and Hannah Buzard. All rights reserved.
 //
 
 import UIKit
--- FitCheck V.0.1/ViewController.swift
+++ FitCheck V.0.1/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  FitCheck V.0.1
 //
-//  Created by David Kipnis on 4/7/21.
-//  Copyright © 2021 David Kipnis. All rights reserved.
+//  Created by David Kipnis and Hannah Buzard on 4/7/21.
+//  Copyright © 2021 David Kipnis and Hannah Buzard. All rights reserved.
 //
 
 import UIKit
@@ -12,29 +12,62 @@ class ViewController: UIViewController {
     
     var isOn: Bool = false
     
-    @IBAction func buttonPress(_ sender: UIButton) {
-        isOn.toggle()
-        print("Button Pressed")
-//        setButtonBackGround(view: sender, on:  UIImage(imageLiteralResourceName: "heart.fill"), off:  UIImage(imageLiteralResourceName: "person.2"), onOffStatus: isOn)
+    @IBAction func animateButton(sender: UIButton) {
+
+        sender.transform = CGAffineTransform(scaleX: 0.85, y: 0.85)
+
+        UIView.animate(withDuration: 0.5,
+                                   delay: 0,
+                                   usingSpringWithDamping: CGFloat(0.20),
+                                   initialSpringVelocity: CGFloat(6.0),
+                                   options: UIView.AnimationOptions.allowUserInteraction,
+                                   animations: {
+                                    sender.transform = CGAffineTransform.identity
+            },
+                                   completion: { Void in()  }
+        )
     }
     
-    func setButtonBackGround(view: UIButton, on: UIImage, off: UIImage, onOffStatus: Bool ) {
-         switch onOffStatus {
-              case true:
-    // Chnage backgroundImage to hart image
-                   view.setImage(on, for: .normal)
-    // Test
-              print("Button Pressed")
-              default:
-                   view.setImage(off, for: .normal)
-                   print("Button Unpressed")
+    @IBAction func socialsButtonPress(_ sender: UIButton) {
+        isOn.toggle()
+        sender.tintColor = UIColor.systemYellow
+        mainButton.tintColor = UIColor.white
+        closetButton.tintColor = UIColor.white
+        likesButton.tintColor = UIColor.white
     }
+    
+    @IBAction func mainButtonPress(_ sender: UIButton) {
+        isOn.toggle()
+        sender.tintColor = UIColor.systemYellow
+        socialsButton.tintColor = UIColor.white
+        closetButton.tintColor = UIColor.white
+        likesButton.tintColor = UIColor.white
     }
-
+    
+    @IBAction func closetButtonPress(_ sender: UIButton) {
+        isOn.toggle()
+        sender.tintColor = UIColor.systemYellow
+        mainButton.tintColor = UIColor.white
+        socialsButton.tintColor = UIColor.white
+        likesButton.tintColor = UIColor.white
+    }
+    
+    @IBAction func likesButtonPress(_ sender: UIButton) {
+        isOn.toggle()
+        sender.tintColor = UIColor.systemYellow
+        mainButton.tintColor = UIColor.white
+        closetButton.tintColor = UIColor.white
+        socialsButton.tintColor = UIColor.white
+    }
+    
+    
+    @IBOutlet weak var socialsButton: UIButton!
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var closetButton: UIButton!
+    @IBOutlet weak var likesButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
 }
 
