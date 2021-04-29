b0b25c61beead04fe8fd0072b49ab6562838f5fe
David Kipnis <pankimatick@gmail.com>
2021-04-28 23:05:16 -0700

    Updated card frame and color - added image to card and moved the like and dislike icon - image is still not fitting entirely to scale

--- FitCheck V.0.1/ViewController.swift
+++ FitCheck V.0.1/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     var isOn: Bool = false
     
+    //animating nav bar button clicks
     @IBAction func animateButton(sender: UIButton) {
 
         sender.transform = CGAffineTransform(scaleX: 0.85, y: 0.85)
@@ -28,12 +29,9 @@ class ViewController: UIViewController {
         )
     }
     
-    @IBAction func buttonPress(_ sender: UIButton) {
+    //clicking the socials page button
+    @IBAction func SocialClick(_ sender: UIButton) {
         isOn.toggle()
-        sender.tintColor = UIColor.systemYellow
-        mainButton.tintColor = UIColor.white
-        closetButton.tintColor = UIColor.white
-        likesButton.tintColor = UIColor.white
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
         let nextViewController = storyBoard.instantiateViewController(withIdentifier: "social") as! SocialsPage
         self.view.window?.rootViewController = nextViewController
@@ -41,10 +39,12 @@ class ViewController: UIViewController {
             nextViewController.presentationController?.presentedView?.gestureRecognizers?[0].isEnabled = false
         })}
     
+    //clicking the home page button
     @IBAction func mainButtonPress(_ sender: UIButton) {
         isOn.toggle()
     }
     
+    //clicking the closet page button
     @IBAction func ClosetClick(_ sender: UIButton) {
         isOn.toggle()
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
@@ -55,6 +55,7 @@ class ViewController: UIViewController {
         })
     }
     
+    //clicking the likes page button
     @IBAction func LikesClick(_ sender: UIButton) {
         isOn.toggle()
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
@@ -63,15 +64,20 @@ class ViewController: UIViewController {
         self.present(nextViewController, animated: true, completion: {
             nextViewController.presentationController?.presentedView?.gestureRecognizers?[0].isEnabled = false
         })}
-    //card Swiping
-    @IBOutlet weak var Card: UIView!
+    
+    //outlets for the card and the two like icons on the card for swiping
+    @IBOutlet weak var Card: UIImageView!
     @IBOutlet weak var likeIcon: UIImageView!
     @IBOutlet weak var dislikeIcon: UIImageView!
+    @IBOutlet weak var navBar: UIView!
+    
+    //card Swiping
     @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        var regenFlag = false
         let card = sender.view!
         let point = sender.translation(in: view)
         let xFromCenter = card.center.x - view.center.x
-        
+            
         if xFromCenter > 0 {
             likeIcon.alpha = xFromCenter / 100
             dislikeIcon.alpha = 0
@@ -80,38 +86,45 @@ class ViewController: UIViewController {
             likeIcon.alpha = 0
         }
         
-        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y - 50)
         
+        //when user stops holding the card, check if it passed boundaries
         if sender.state == UIGestureRecognizer.State.ended {
-            
             if card.center.x < 75 {
             //move card off screen to the left
-                UIView.animate(withDuration: 0.6, animations: {
+                UIView.animate(withDuration: 0.2, animations: {
                     card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
+                    card.transform = card.transform.rotated(by: -.pi/10)
                     card.alpha = 0
                 })
-                UIView.animate(withDuration: 0.6, animations: {
-                    card.center = CGPoint(x: self.view.center.x, y: self.view.center.y)
-                })
-                return
+                regenFlag = true
             } else if card.center.x > (view.frame.width - 75) {
             //move card off screen to the right
-                UIView.animate(withDuration: 0.6, animations: {
+                UIView.animate(withDuration: 0.2, animations: {
                     card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
+                    card.transform = card.transform.rotated(by: .pi/10)
                     card.alpha = 0
                 })
-                UIView.animate(withDuration: 0.6, animations: {
-                    card.center = CGPoint(x: self.view.center.x, y: self.view.center.y)
-                })
-                return
+                regenFlag = true
             }
             
-            
+            if regenFlag == false {
             UIView.animate(withDuration: 0.2, animations: {
-                card.center = self.view.center
+                card.center = CGPoint(x: self.navBar.center.x, y: self.view.center.y - 50)
                 self.likeIcon.alpha = 0
                 self.dislikeIcon.alpha = 0
             })
+            } else {
+//                card.alpha = 0
+//                UIView.animate(withDuration: 0, animations: {
+//                    card.center = CGPoint(x: self.view.center.x, y: self.view.center.y)
+//                })
+//                UIView.animate(withDuration: 1.0, animations: {
+//                    card.alpha = 1
+//                })
+//                self.likeIcon.alpha = 0
+//                self.dislikeIcon.alpha = 0
+            }
         }
     }
     
@@ -125,6 +138,17 @@ class ViewController: UIViewController {
         self.navigationController?.navigationBar.isHidden = true
         animateButton(sender: mainButton)
         mainButton.tintColor = UIColor.systemYellow
+        
+        //generating and adding image to first card
+        let image = UIImage(named: "test_image")
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 0, y: 0, width: 314, height: 540)
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        self.Card.addSubview(imageView)
+        Card.bringSubviewToFront(likeIcon)
+        Card.bringSubviewToFront(dislikeIcon)
     }
 }
 
