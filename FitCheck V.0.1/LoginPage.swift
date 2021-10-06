f872ad70caa5f4fd5932976335a82e6e511c39dc
Hannah <hannahbuzard@icloud.com>
2021-10-06 16:30:53 -0700

    Firebase Authentication hooked up. Login page (view) created. Still need to: Connect button from view to code to capture user input for login (will do tonight 10/6).

---
+++ FitCheck V.0.1/LoginPage.swift
@@ -0,0 +1,20 @@
+//
+//  SocialsPage.swift
+//  FitCheck V.0.1
+//
+//  Created by Hannah Buzard on 4/13/21.
+//  Copyright © 2021 David Kipnis. All rights reserved.
+//
+
+
+import UIKit
+ 
+class LoginPage: UIViewController {
+    
+    override func viewDidLoad() {
+        print("login page Loaded")
+    }
+    
+
+}
+
